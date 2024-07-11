f1e4fc9b7652eb98d5151614ebd8c202959cd76d
whisper3zzz <3194338947@qq.com>
2024-07-11 13:44:40 +0800

    Unitask

--- Assets/Script/Utility/SingletonPatternSystem/MonoSingleton.cs
+++ Assets/Script/Utility/SingletonPatternSystem/MonoSingleton.cs
@@ -5,12 +5,12 @@ namespace Utility.SingletonPatternSystem
 {
     public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
     {
-        
         public static T Instance { get; private set; } = null;
 
         protected virtual void Awake()
         {
             Instance = (T)this;
+            DontDestroyOnLoad(gameObject);
         }
     }
 }
